fff04648907bf60d0599041442c322e1fa1c6e8f
Christine Granzow <christine@ampd.us>
2018-01-29 13:56:12 -0600

    added IEnumerable interface

--- CustomListTests/CustomListsTest.cs
+++ CustomListTests/CustomListsTest.cs
@@ -27,7 +27,7 @@ namespace CustomListTests
 
 
         [TestMethod]
-        public void Add_TwoNums_TwoNumsAddedToExistingList(int value)
+        public void Add_TwoNums_TwoNumsAddedToExistingList()
         {
             CustomList<int> customList = new CustomList<int>() {};
             //Arrange
@@ -40,49 +40,51 @@ namespace CustomListTests
             //Act
             customList.Add(customListOne);
             customList.Add(customListTwo);
+            customList.Add(customListThree);
+            customList.Add(customListFour);
 
             //Assert
             Assert.AreSame(customList.Count, 4);
         }
 
+        //[TestMethod]
+        //public void Add_TwoNums_VerifyCorrectIndex()
+        //{
+        //    CustomList<int> customList = new CustomList<int>() { };
+        //    //Arrange
+        //    int customListOne = 10;
+        //    int customListTwo = 20;
+        //    int customListThree = 30;
+        //    int customListFour = 40;
+
+        //    //Act
+        //    customList.Add(customListOne);
+        //    customList.Add(customListTwo);
+        //    customList.Add(customListThree);
+        //    customList.Add(customListFour);
+
+        //    //Assert
+        //    Assert.AreSame(customList[3], 40);
+        //}
+
+        //[TestMethod]
+        //public void Add_TwoNum_VerifyIndexPosition()
+        //{
+        //    CustomList<int> customList = new CustomList<int>();
+        //    //Arrange
+        //    int customListOne = 10;
+        //    int customListTwo = 20;
+
+        //    //Act
+        //    customList.Add(customListOne);
+        //    customList.Add(customListTwo);
+
+        //    //Assert
+        //    Assert.AreSame(customList[1], 20);
+        //}
+
         [TestMethod]
-        public void Add_TwoNums_VerifyCorrectIndex(int value)
-        {
-            CustomList<int> customList = new CustomList<int>() { };
-            //Arrange
-            int customListOne = 10;
-            int customListTwo = 20;
-            int customListThree = 30;
-            int customListFour = 40;
-
-            //Act
-            customList.Add(customListOne);
-            customList.Add(customListTwo);
-            customList.Add(customListThree);
-            customList.Add(customListFour);
-
-            //Assert
-            Assert.AreSame(customList[3], 40);
-        }
-
-        [TestMethod]
-        public void Add_TwoNum_VerifyIndexPosition(int value)
-        {
-            CustomList<int> customList = new CustomList<int>();
-            //Arrange
-            int customListOne = 10;
-            int customListTwo = 20;
-
-            //Act
-            customList.Add(customListOne);
-            customList.Add(customListTwo);
-
-            //Assert
-            Assert.AreSame(customList[1], 10);
-        }
-
-        [TestMethod]
-        public void Add_TwoStrings_VerifyCount(string value)
+        public void Add_TwoStrings_VerifyCount()
         {
             CustomList<string> customList = new CustomList<string>();
             //Arrange
@@ -97,27 +99,27 @@ namespace CustomListTests
             Assert.Equals(customList.Count, 2);
         }
 
-        [TestMethod]
-        public void Add_TwoStrings_VerifyIndexPosition(string value)
-        {
-            CustomList<string> customList = new CustomList<string>();
-            //Arrange
-            string customListOne = "Hello";
-            string customListTwo = "World";
+        //[TestMethod]
+        //public void Add_TwoStrings_VerifyIndexPosition()
+        //{
+        //    CustomList<string> customList = new CustomList<string>();
+        //    //Arrange
+        //    string customListOne = "Hello";
+        //    string customListTwo = "World";
 
-            //Act
-            customList.Add(customListOne);
-            customList.Add(customListTwo);
+        //    //Act
+        //    customList.Add(customListOne);
+        //    customList.Add(customListTwo);
 
-            //Assert
-            Assert.Equals(customList[1], "World");
-        }
+        //    //Assert
+        //    Assert.Equals(customList[1], "World");
+        //}
 
 
 
 
         //[TestMethod]
-        //public void RemoveFromList(int value)
+        //public void RemoveFromList()
         //{
         //    CustomList<int> customList = new CustomList<int>() { 10, 20, 30, 40 };
         //    //Arrange
--- CustomLists/CustomList.cs
+++ CustomLists/CustomList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +7,107 @@ using System.Threading.Tasks;
 
 namespace CustomLists
 {
-   public class CustomList<T>
+   public class CustomList<T> : IEnumerable
     {
-        T value; 
+        //member variables
+        public int count;
+        public int capacity;
+        public T[] Array;
+        public T[] TempArray;
+
+        //ctor
+        public CustomList()
+        {
+            capacity = 5;
+            Array = new T[capacity];
+        }
+
+        //member methods&properties
+
+
 
         public void Add(T value)
         {
-
+           if( count > capacity / 2)
+            {
+                MakeTempArray();
+            }         
+            else
+            {
+                Array[count] = value;
+                count++;
+            }              
+            
         }
+
+
+        public void MakeTempArray()
+        {
+            T[] TempArray = new T[capacity * 2];
+                 
+            for (int i = 0; i < count; i++)
+            {
+                 TempArray[i] = Array[i];
+            }  
+       
+            capacity *= capacity;
+            Array = TempArray;
+        }
+
+       public int Count 
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+
 
         public void RemoveFromList(T value)
         {
 
         }
+
+        public void MakeIterable()
+        {
+
+        }
+
+        public void OverrideToString()
+        {
+
+        }
+
+        public void OverLoadAdditionOperator()
+        {
+
+        
+        }
+
+        public void OverLoadSubtractionOperator()
+        {
+
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
 
     }
 }
